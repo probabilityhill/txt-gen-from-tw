f17f087e2e6884bcf99384fb5abba638cd88a425
probabilityhill <probabilityhill.rubedo@gmail.com>
2022-04-19 23:48:54 +0900

    total:1097tw

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ USER_ID = "77907829"
 tweet_data = []
 # until_id=""
 
-for tweet in tweepy.Paginator(client.get_users_tweets, id=USER_ID, max_results=100, exclude="retweets", until_id="1479064202580873216").flatten(limit=385):
+for tweet in tweepy.Paginator(client.get_users_tweets, id=USER_ID, max_results=100, exclude="retweets", until_id="1474357125690576897").flatten(limit=100):
     tweet_data.append([tweet.id, tweet.text.replace("\n", "<n>")])
 
 print("\n-------------------------------\n".join(["Tweet ID: "+str(x[0])+"\n"+x[1] for x in tweet_data]))
