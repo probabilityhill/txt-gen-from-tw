764af6e9d45587775564b4c4872af90c7c62e3f7
probabilityhill <probabilityhill.rubedo@gmail.com>
2022-04-19 23:36:09 +0900

    csv_to_txtを作成

---
+++ csv_to_text.py
@@ -0,0 +1,12 @@
+CSV_PATH = "tweets.csv"
+TXT_PATH = "tweets.txt"
+
+tweets = []
+with open(CSV_PATH, mode="r", encoding="utf-8") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        tweets.append([row[1] + "<|endoftext|>"])
+
+with open(TXT_PATH, mode="w", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerows(tweets)
